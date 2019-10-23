5ce085d9039edda2e2d8b864cd777f30ee9bd8a4
Morne Odendaal <morne@jemstep.com>
2019-10-23 14:09:15 +0200

    Changed class name and added more code

---
+++ src/main/scala/ToTestOrNotToTest.scala
@@ -0,0 +1,13 @@
+object ToTestOrNotToTest extends App {
+  def cube(x: Int) = {
+    x * x * x
+  }
+
+  def add(x: Int, y: Int) = {
+    x + y
+  }
+
+  def multiply(x: Int, y: Int) = {
+    x * y
+  }
+}
---
+++ src/test/scala/ToTestOrNotToTestSpec.scala
@@ -0,0 +1,5 @@
+class ToTestOrNotToTestSpec extends org.scalatest.FunSuite {
+  test("CubeCalculator.cube") {
+    assert(ToTestOrNotToTest.cube(3) === 27)
+  }
+}
