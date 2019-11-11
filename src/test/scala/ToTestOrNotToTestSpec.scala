da07abf924dd9c8eb4dd899f22778df81f0eaa39
samina <Sabimahomed@gmail.com>
2019-11-11 17:24:37 +0200

    Updated Test cases with negative values

--- src/test/scala/ToTestOrNotToTestSpec.scala
+++ src/test/scala/ToTestOrNotToTestSpec.scala
@@ -6,8 +6,11 @@ class ToTestOrNotToTestSpec extends org.scalatest.FunSuite {
   }
   test("Testing the add function") {
     assert(ToTestOrNotToTest.add(5,3) === 8)
+    assert(ToTestOrNotToTest.add(-5,-3) === -8)
   }
   test("Testing the multiply function") {
     assert(ToTestOrNotToTest.multiply(3,9) === 27)
+    assert(ToTestOrNotToTest.multiply(-3,-9) === 27)
+    assert(ToTestOrNotToTest.multiply(-3,9) === -27)
   }
 }
