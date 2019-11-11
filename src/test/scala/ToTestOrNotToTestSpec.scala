7968cfd4338825406eeffd26e3b9764b4c1f66fc
samina <Sabimahomed@gmail.com>
2019-11-11 13:00:09 +0200

    Adding test for Milestone2

--- src/test/scala/ToTestOrNotToTestSpec.scala
+++ src/test/scala/ToTestOrNotToTestSpec.scala
@@ -1,5 +1,13 @@
 class ToTestOrNotToTestSpec extends org.scalatest.FunSuite {
   test("Testing the cube function") {
     assert(ToTestOrNotToTest.cube(3) === 27)
+    assert(ToTestOrNotToTest.cube(2) === 8)
+    assert(ToTestOrNotToTest.cube(-2) === -8)
+  }
+  test("Testing the add function") {
+    assert(ToTestOrNotToTest.add(5,3) === 8)
+  }
+  test("Testing the multiply function") {
+    assert(ToTestOrNotToTest.multiply(3,9) === 27)
   }
 }
